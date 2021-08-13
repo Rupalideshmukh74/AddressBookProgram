41f70d4b6bd127ffdb6d378caa86938ffde0c497
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-13 17:50:56 +0530

    [Rupali]Add:Welcome message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AddressBookProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Management System Development!");
+            Console.WriteLine("******************************************************");
+
+            //UserInput Class Object
+            UserInput userInput = new UserInput();
+        }
+    }
+}
+        
+    
+
