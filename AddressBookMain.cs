b298c478a86c4704bdcd729bc7cb5fe9b618a20b
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 16:58:03 +0530

    [Rupali]Feat:Added:Contact created

---
+++ AddressBookMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookDAY10
+{
+    class AddressBookMain
+    {
+        public static List<Person> ContactList = new List<Person>();
+
+        public void Display()
+        {
+            Console.WriteLine(" Welcome to Address Book Program \n");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace AddressBookDAY10
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Program!");
+            AddressBookMain abc = new AddressBookMain();
         }
     }
 }
